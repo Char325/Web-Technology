3653f59a2fdf4050e42a8f9cc7f1e9b7c006f26a
Char325 <111283967+Char325@users.noreply.github.com>
2023-11-04 14:37:59 +0530

    Create Dash.jsx

---
+++ Lab-5/components/Dash.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Dash(){
+    return(
+        <div className="dash">
+        <div className="texts">
+          <h1 style={{paddingTop:"20px"}}> Welcome to Book Club!</h1>
+          <h3 >Hello book lover! We plan on helping readers all over the globe by providing easy access to reviews of whichever books they feel like reading. 
+          If you like the book, all you have to do is click on the book cover and you'll be directy taken to reviews.
+          Happy reading!</h3>
+        </div>
+        </div>
+    );
+}
+
+export default Dash;
