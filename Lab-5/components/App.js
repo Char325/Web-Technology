6549b82784976e8ef0ae1f898467bf77af39ef55
Char325 <111283967+Char325@users.noreply.github.com>
2023-11-04 14:36:30 +0530

    Create App.js

---
+++ Lab-5/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Dash from "./Dash";
+import BookList from "./BookList";
+function App() {
+  return (
+    <div className="App">
+      <Dash/>
+      <BookList/>
+    </div>
+  );
+}
+
+export default App;
