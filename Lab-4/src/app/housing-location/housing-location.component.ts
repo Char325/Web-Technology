61fe286afc0e91ff718fad28f0d46ac8a4369e94
Char325 <111283967+Char325@users.noreply.github.com>
2023-11-04 14:19:40 +0530

    Create housing-location.component.ts

---
+++ Lab-4/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }} by {{ housingLocation.author}}</h2>
+      <p class="listing-location"> <b>DESC:</b>{{ housingLocation.desc}}</p>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
+
+
