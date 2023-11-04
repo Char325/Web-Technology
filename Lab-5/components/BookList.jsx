65549816c32db779c93c83b9f389c2c987fa1b40
Char325 <111283967+Char325@users.noreply.github.com>
2023-11-04 14:37:05 +0530

    Create BookList.jsx

---
+++ Lab-5/components/BookList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+function BookList(){
+    
+    return(
+        <div className="list">
+            <h1 >Find Books Here!</h1>
+            <Card/>
+            
+        </div>
+    )
+}
+
+export default BookList;
